89566a1b85c4c327e57d53e3e7a8e019ff24843d
martin <martin@cexbit.com>
2022-09-13 15:29:04 +0200

    init app autoreloading

--- src/main/kotlin/com/example/models/Data.kt
+++ src/main/kotlin/com/example/models/Data.kt
@@ -34,5 +34,11 @@ val dataStorage = listOf(
         4,
         "user2",
         "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+    ),
+    Data(
+        "4",
+        4,
+        "user1",
+        "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
     )
 )
